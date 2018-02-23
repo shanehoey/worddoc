1660453fa63f8dbab4a6fbc90c37e96995199ac3
shanehoey <shanehoey@users.noreply.github.com>
2018-02-23 23:48:29 +1000

    updated template

--- Scripts/templateDoc.ps1
+++ Scripts/templateDoc.ps1
@@ -58,7 +58,7 @@ Param(
     try { import-module -name WordDoc -ErrorAction Stop } catch { Write-Warning "WordDoc Module is required , to install ->  install-module -name worddoc -scope currentuser"; break }
 
     if(!($DesignJsonURL)) { $DesignJsonURL ='https://shanehoey.com/templatedoc.json' }    #Change this 
-    $VersionScript = "523129c9-3f83-4efc-839f-973c2c205a28"                         #Change this (new-guid)
+    $VersionGUID = "523129c9-3f83-4efc-839f-973c2c205a28"                         #Change this (new-guid)
     $VersionURL = 'https://shanehoey.com/versions/templateDoc.json'                 #Change this 
     $useragent = 'templateDoc'                                                      #Change this
     
@@ -116,11 +116,10 @@ Param(
     # It is only used for version Control and unique users via github 
     # Collecting the stats gives me an indication how often this script is used to determine if I should continue developing it, or concentrate on other projects
     # If you want to silence the notice set notify to $false rather than deleting the section
-    # thank in advance
     try 
     {
         $Version = (Invoke-WebRequest -Uri $VersionURL -UserAgent $useragent -Method Get -DisableKeepAlive -TimeoutSec 2 ).content | convertfrom-json 
-        if (($thisversion -ne $version.release) -and ($thisversion -ne $version.dev)) 
+        if (($VersionGUID -ne $version.release) -and ($VersionGUID -ne $version.dev)) 
         {
             if ($notifyupdates) 
             { 
@@ -135,78 +134,66 @@ Param(
     }
     #endregion
 
-    #region Create Word Document 
-        New-WordInstance 
-        New-WordDocument
-        if ($TemplateFile) {
-            Add-WordTemplate -filename $TemplateFile
-        }
-    #endregion
+    New-WordInstance 
+    New-WordDocument
+    if ($TemplateFile) {
+        Add-WordTemplate -filename $TemplateFile
+    }
 
-    #Turn of spelling to speed up creating doc
-    #TODO - Need to work out how to turn spelling on/off on a per document basis. 
-    (get-wordInstance).options.checkspellingasyoutype = $false
-
-    #region Coverpage
     if($section.CoverPage) { 
         
-        #region AddCoverpage
-            for ($i = 0; $i -lt 3; $i++) 
-            {
-                Add-WordBreak -breaktype Paragraph 
-            }
-            Add-WordText -text $DocumentTitle -WDBuiltinStyle wdStyleTitle
-            for ($i = 0; $i -lt 3; $i++) 
-            {
-                Add-WordBreak -breaktype Paragraph 
-            }
-            Add-WordText -text 'for' -WDBuiltinStyle wdStyleTitle
-            for ($i = 0; $i -lt 3; $i++) 
-            {
-                Add-WordBreak -breaktype Paragraph 
-            }
-            Add-WordText -text $DocumentCustomer -WDBuiltinStyle wdStyleTitle
-            Add-WordBreak -breaktype NewPage
-        #endregion 
-
-        #region License
-            $license = "MIT License`nCopyright (c) 2016-2018 Shane Hoey`rPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:`nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.`nTHE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
-            Add-WordBreak -breaktype Paragraph
-            Add-WordText -text 'This document has been created with wordDoc which has been distributed under the MIT license. For more information visit http://shanehoey.github.io/worddoc/' -WDBuiltinStyle wdStyleBookTitle
-            Add-WordBreak -breaktype Paragraph
-            #bug with bold/italic in worddoc module
-            $selection = (Get-WordDocument).application.selection
-            $selection.font.Bold = $False
-            $selection.ParagraphFormat.Alignment = 3
-            Add-WordText -text $license -WDBuiltinStyle wdStyleNormal
-            Add-WordBreak -breaktype NewPage
-        #endregion
-
-        #region Shameless Plug
+        #Add Coverpage
         for ($i = 0; $i -lt 3; $i++) 
         {
-        Add-WordBreak -breaktype Paragraph 
+            Add-WordBreak -breaktype Paragraph 
+        }
+        Add-WordText -text $DocumentTitle -WDBuiltinStyle wdStyleTitle
+        for ($i = 0; $i -lt 3; $i++) 
+        {
+            Add-WordBreak -breaktype Paragraph 
+        }
+        Add-WordText -text 'for' -WDBuiltinStyle wdStyleTitle
+        for ($i = 0; $i -lt 3; $i++) 
+        {
+            Add-WordBreak -breaktype Paragraph 
+        }
+        Add-WordText -text $DocumentCustomer -WDBuiltinStyle wdStyleTitle
+        Add-WordBreak -breaktype NewPage
+
+        #AddLicense
+        $license = "MIT License`nCopyright (c) 2016-2018 Shane Hoey`rPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:`nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.`nTHE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
+        Add-WordBreak -breaktype Paragraph
+        Add-WordText -text 'This document has been created with wordDoc which has been distributed under the MIT license. For more information visit http://shanehoey.github.io/worddoc/' -WDBuiltinStyle wdStyleBookTitle
+        Add-WordBreak -breaktype Paragraph
+        #bug with bold/italic in worddoc module
+        $selection = (Get-WordDocument).application.selection
+        $selection.font.Bold = $False
+        $selection.ParagraphFormat.Alignment = 3
+        Add-WordText -text $license -WDBuiltinStyle wdStyleNormal
+        Add-WordBreak -breaktype NewPage
+
+        #Add Shameless Plug
+        for ($i = 0; $i -lt 3; $i++) 
+        {
+            Add-WordBreak -breaktype Paragraph 
         }
         Add-WordText -text 'Are you using this commercially? Show your appreciation and encourage more development of this script at https://paypal.me/shanehoey' -WDBuiltinStyle wdStyleIntenseQuote
-        #endregion
 
-        #region TOC
-            Add-WordBreak -breaktype NewPage
-            Add-WordText -text 'Contents' -WDBuiltinStyle wdStyleTOCHeading 
-            Add-WordTOC 
-            Add-WordBreak -breaktype NewPage
-        #endregion 
+        #Table of Contents
+        Add-WordBreak -breaktype NewPage
+        Add-WordText -text 'Contents' -WDBuiltinStyle wdStyleTOCHeading 
+        Add-WordTOC 
+        Add-WordBreak -breaktype NewPage
 
-        #region update document settings
-            Set-WordBuiltInProperty -WdBuiltInProperty wdPropertytitle -text $DocumentTitle
-            Set-WordBuiltInProperty -WdBuiltInProperty wdPropertySubject -text "$Documenttitle for $documentCustomer"
-            Set-WordBuiltInProperty -WdBuiltInProperty wdPropertyAuthor -text $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('UwBoAGEAbgBlACAASABvAGUAeQA=')))
-            Set-WordBuiltInProperty -WdBuiltInProperty wdPropertyComments -text $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('aAB0AHQAcABzADoALwAvAHMAaABhAG4AZQBoAG8AZQB5AC4AZwBpAHQAaAB1AGIALgBpAG8ALwB3AG8AcgBkAGQAbwBjAC8A')))
-            Set-WordBuiltInProperty -WdBuiltInProperty wdPropertyManager -text $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('UwBoAGEAbgBlACAASABvAGUAeQA=')))
-        #endregion
+        #Update Document Settings
+        Set-WordBuiltInProperty -WdBuiltInProperty wdPropertytitle -text $DocumentTitle
+        Set-WordBuiltInProperty -WdBuiltInProperty wdPropertySubject -text "$Documenttitle for $documentCustomer"
+        Set-WordBuiltInProperty -WdBuiltInProperty wdPropertyAuthor -text $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('UwBoAGEAbgBlACAASABvAGUAeQA=')))
+        Set-WordBuiltInProperty -WdBuiltInProperty wdPropertyComments -text $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('aAB0AHQAcABzADoALwAvAHMAaABhAG4AZQBoAG8AZQB5AC4AZwBpAHQAaAB1AGIALgBpAG8ALwB3AG8AcgBkAGQAbwBjAC8A')))
+        Set-WordBuiltInProperty -WdBuiltInProperty wdPropertyManager -text $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('UwBoAGEAbgBlACAASABvAGUAeQA=')))
 
     }
-    #endregion 
+
     if ($section.Overview) 
     {
         Add-WordText "Overview" -WDBuiltinStyle wdStyleHeading1 
