a42e9dcd400828838cd4feb0dd847805c0349b74
shanehoey <shanehoey@users.noreply.github.com>
2018-03-14 23:50:25 +1000

    updated build

--- Scripts/build.ps1
+++ Scripts/build.ps1
@@ -23,12 +23,24 @@ Test-ModuleManifest -path ".\WordDoc\WordDoc.psd1"
 Remove-Module WordDoc -ErrorAction SilentlyContinue
 Import-Module .\WordDoc\WordDoc.psd1 
 
+New-WordInstance
+New-WordDocument
+
+$fa_github  = [char]0xf09b
+$fontawesometext = "Font Awesome 5 Brands Regular"
+add-wordtext  -text $fa_github -Font $fontawesometext -Size 45 -NoParagraph
+add-wordtext "https://shanehoey.github.io/worddoc/"
+
+Close-WordDocument -SaveOptions wdDoNotSaveChanges
+Close-WordDocument
+Close-WordInstance
+
 get-command -Module WordDoc | select name,version
 
 #Manually run these 
-code .\Scripts\example-1-simple.ps1
-code .\Scripts\example-2-detailed.ps1
-code .\Scripts\example-3-template.ps1
+. .\Scripts\example-1-simple.ps1
+. .\Scripts\example-2-detailed.ps1
+. .\Scripts\example-3-template.ps1
 
 ### MANUAL GitHUB Commit to master
 
